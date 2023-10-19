609d624473c592c77bd8c7854a550cedb285d5a4
artemtsvietkov <143085970+artemtsvietkov@users.noreply.github.com>
2023-10-19 11:47:11 +0200

    Guess number

--- GissaTalet/GissaTalet.py
+++ GissaTalet/GissaTalet.py
@@ -6,25 +6,43 @@ user_answer = 0
 correct_answer = random.randint(1, 100)
 print("Hello, let's try guessing a number 1-100")
 print("You have 7 attempts")
-while True:
-    try:
-        user_answer = int(input("Guess the number! "))
+running = True
+    
+while running:
+    while True:
+        try:
+            user_answer = int(input("Guess the number! "))
+        except ValueError: 
+            print("Type in only numbers! You didn't lose your attempt this time!")     
         if 1 == number_of_attempts: 
             print("You have no more attempts")
-            break
+            print("The number was",correct_answer)
+            restart_game = input("Do you want to try again? Yes/No ")                
+            if restart_game == "Yes,yes,YEs":
+                running
+            elif restart_game == "No,no,nO":
+                running = False
+            else:
+                break
         else:
             if user_answer == correct_answer:
                 user_attempts =+ 1 
-                print("Congratulations, the number was", correct_answer, "you won! And you needed", user_attempts,"attempts!")
                 break
+                
             elif user_answer > correct_answer:
                 number_of_attempts = number_of_attempts -1
-                user_attempts =+ 1 
+                user_attempts = user_attempts + 1
                 print("Too high! You have",number_of_attempts," attempts left!")
             else: 
                 number_of_attempts = number_of_attempts -1
-                user_attempts += 1
+                user_attempts = user_attempts + 1
                 print("Too low! You have",number_of_attempts, "attempts left!")
             
-    except ValueError: 
-        print("Type in only numbers! You didn't lose your attempt this time!")     
+    print("Congratulations, the number was", correct_answer, "you won! And you needed", user_attempts,"attempts!")
+    restart_game = input("Do you want to try again? Yes/No").upper()                
+    if restart_game == "YES":
+        continue
+    elif restart_game == "NO":
+        running = False
+    else:
+        running = False
